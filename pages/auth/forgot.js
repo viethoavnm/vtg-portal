9e876ba5e138a2e9edbb5e7926c406b9f9e71e82
viethoavnm <viethoavnm@gmail.com>
2019-01-06 17:58:52 +0700

    fix bablerc

--- pages/auth/forgot.js
+++ pages/auth/forgot.js
@@ -1,2 +1,2 @@
 import dynamic from 'next/dynamic'
-export default dynamic({ loader: () => import('../../app-modules/auth/Forgot') })
+export default dynamic({ loader: () => import('app-modules/auth/Forgot') })
