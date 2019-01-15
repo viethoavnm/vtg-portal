7a8c0f31d3a9ee6f89e96b2c022dde974775ba45
viethoavnm <viethoavnm@gmail.com>
2019-01-15 07:01:50 +0700

    fix fr-view

--- app-modules/docs/Docs.js
+++ app-modules/docs/Docs.js
@@ -1,7 +1,6 @@
-import React from 'react'
-import request from 'api'
-import Head from 'next/head'
-import { Skeleton } from 'antd'
+import React from 'react';
+import request from 'api';
+import { Skeleton } from 'antd';
 
 class Docs extends React.Component {
   state = { content: '', loading: false }
@@ -15,9 +14,6 @@ class Docs extends React.Component {
   render() {
     return (
       <div className="docs-page">
-        <Head>
-          <link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.1/css/froala_style.min.css" rel="stylesheet" type="text/css" />
-        </Head>
         <div className="container">
           <div dangerouslySetInnerHTML={{ __html: this.state.content }} className="fr-view" />
           {this.state.loading && <Skeleton active paragraph={{ rows: 10 }} />}
